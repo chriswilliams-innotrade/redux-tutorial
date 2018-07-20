ad9ecb4b6b79e19d3cabf5bdf26a20c28f52e882
Chris Williams <chris@innotrade.io>
2018-07-19 23:03:38 -0600

    add updateUser action

--- src/index.js
+++ src/index.js
@@ -12,6 +12,11 @@ function productsReducer(state = [], action) {
 }
 
 function userReducer(state = '', action) {
+    switch (action.type) {
+        case 'updateUser':
+            return action.payload;
+    }
+    
     return state;
 }
 
@@ -20,9 +25,25 @@ const allReducers = combineReducers({
     user: userReducer
 });
 
-const store = createStore(allReducers);
+const store = createStore(
+    allReducers,
+    {
+        products: [{ name: 'iPhone'}],
+        user: 'Michael'
+    },
+    window.devToolsExtension && window.devToolsExtension()
+);
 
 console.log(store.getState())
+
+const updateUserAction = {
+    type: 'updateUser',
+    payload: {
+        user: 'John'
+    }
+};
+
+store.dispatch(updateUserAction)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
